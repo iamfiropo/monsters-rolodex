73f174806f147a83009ca4f5da4aefb3bfa3cf8c
Ropo John Olatujoye <princeolatjohnjoe@yahoo.com>
2019-08-19 13:58:58 -0400

    Updates

---
+++ precache-manifest.712ff7e2d3cc9229544e8d7c5bd325e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a50926971f8518bdb2648e0d3ffed2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "68da46d6dd27924d0d8a",
+    "url": "/monsters-rolodex/static/css/main.97b79ebd.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "68da46d6dd27924d0d8a",
+    "url": "/monsters-rolodex/static/js/main.88c754e1.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
