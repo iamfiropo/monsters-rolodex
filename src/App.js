435c4dd4d400fbac32f8ad58c228c92c39e979e4
Ropo John Olatujoye <princeolatjohnjoe@yahoo.com>
2019-08-19 14:05:46 -0400

    change insecure request to https

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(monsters => this.setState({ monsters }))
   }
